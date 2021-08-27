049149373d7bf02a8a2e92dd85a1b2792296d156
Chaitanya Reddy <lit2019034@iiitl.ac.in>
2021-08-27 14:12:35 +0530

    background changed

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -6,9 +6,6 @@ const Login = () => {
   return (
     <main className="main-login">
       <section className="image-section">
-        l;sadfo adfka s;dlkf aosidjf oaidjkf oasdifjoaisdjfoaiwejkdsf
-        oisdajfoakdnf kadflkadkl dfj djfakldjfoadjfoajfoadjfoaidjflakdjfladijf
-        oaeifjadsoifjao idjfoaidj aoijwoei
       </section>
       <section className="login-form-section">
         <LoginForm />
